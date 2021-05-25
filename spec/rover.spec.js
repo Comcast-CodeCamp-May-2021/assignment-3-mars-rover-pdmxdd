3ba204327240399d9a8dcf9de68bbab0e8b2c8e7
Paul Matthews <>
2021-05-25 20:18:30 +0000

    test 11 or 12 in the writeup, but then pivoted so I could demo the assignment to students. gonna have to delete things, and don't want to lose my spot

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -9,5 +9,59 @@ const Command = require('../command.js');
 describe("Rover class", function() {
 
   // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+    const testObj = new Rover(98382);
+    expect(testObj.mode)
+      .toEqual("NORMAL");
+    expect(testObj.generatorWatts)
+      .toEqual(110);
+  })
+
+  it("response returned by receiveMessage contains name of message", function() {
+    const testCommand = new Command("MOVE", 20);
+    const testMessage = new Message("Move message", [testCommand]);
+    const testObj = new Rover("");
+    expect(testObj.receiveMessage(testMessage).name)
+      .toEqual("Move message");
+  })
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+    const testCommandOne = new Command("MOVE", 20);
+    const testCommandTwo = new Command("MOVE", 40);
+    const testMessage = new Message("move message", [testCommandOne, testCommandTwo]);
+    const testObj = new Rover(98382);
+    expect(testObj.receiveMessage(testMessage).commands)
+      .toEqual([testCommandOne, testCommandTwo]);
+  })
+
+  it("responds correctly to status check command", function() {
+    const testCommand = new Command("STATUS_CHECK");
+    const testMessage = new Message("status check message", [testCommand]);
+    const testObj = new Rover(98382);
+    expect(testObj.receiveMessage(testMessage).name)
+      .toEqual("status check message");
+    expect(testObj.receiveMessage(testMessage).commands)
+      .toEqual([testCommand]);
+    expect(testObj.receiveMessage(testMessage).result.mode)
+      .toEqual("NORMAL");
+    expect(testObj.receiveMessage(testMessage).result.generatorWatts)
+      .toEqual(110);
+    expect(testObj.receiveMessage(testMessage).result.position)
+      .toEqual(98382);
+  })
+
+  it("responds correctly to mode change command", function() {
+    const testCommand = new Command("MODE_CHANGE", "LOW_POWER");
+    const testMessage = new Message("message name", [testCommand]);
+    const testRover = new Rover(98382);
+    const testRoverResult = testRover.receiveMessage(testMessage);
+    expect(testRoverResult.result.completed)
+      .toEqual(true);
+
+  })
+
+  it("responds with false completed value when attempting to move in LOW_POWER mode", function() {
+    const testCommandOne = new Command("MODE_CHANGE", "LOW_POWER");
+  })
 
 });
